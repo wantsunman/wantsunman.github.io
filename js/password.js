b737d77d7a61883392341d97fe1c924fc650582d
raymondwan <tmwan5@gapps.cityu.edu.hk>
2016-03-30 18:42:06 +0800

    Added files via upload

---
+++ js/password.js
@@ -0,0 +1,21 @@
+
+<HEAD>
+
+<script type="text/javascript">
+
+var password;
+var pass1 = "AST2014";
+
+password=prompt("Please enter the password to view the content:","");
+
+if (password==pass1) {
+  window.location= "index.html";
+} else {
+  window.location= "wrongpw.html";
+}
+
+</script>
+
+<p><center>
+<font face="arial, helvetica" size"-2">Free JavaScripts provided<br></font>
+</center><p>
